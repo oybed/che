9f08b88ed9dd277e4718b387859eee0f6a5f3534
Øystein Bedin <oybed@users.noreply.github.com>
2023-05-09 11:12:56 -0600

    Update api-test-php.sh

--- tests/performance/api-tests/api-test-php.sh
+++ tests/performance/api-tests/api-test-php.sh
@@ -12,7 +12,7 @@ export containerName="tools"
 export commandToTest="cd /projects/$projectName; php hello-world.php >> command_log.txt; grep '$expectedCommandOutput' ./command_log.txt;"
 
 oc login -u $OCP_USERNAME -p $OCP_PASSWORD --server=$OCP_SERVER_URL --insecure-skip-tls-verify
-oc new-project $OCP_USERNAME-test
+oc new-project ${OCP_USERNAME}-test
 cd /tmp
 
 startWorkspace ${BASE_URL} ${TEST_DEVFILE_PATH} ${WORKSPACE_NAME}
